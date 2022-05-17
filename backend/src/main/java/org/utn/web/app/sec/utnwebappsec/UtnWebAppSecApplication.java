a5905cae851ccf7f1994fa6f8d7a802ccc04e491
Francisco Enzo Di Giorgio <f.e.digiorgio@gmail.com>
2022-05-16 21:28:53 -0300

    Add backend Java Spring project

---
+++ backend/src/main/java/org/utn/web/app/sec/utnwebappsec/UtnWebAppSecApplication.java
@@ -0,0 +1,13 @@
+package org.utn.web.app.sec.utnwebappsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtnWebAppSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtnWebAppSecApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/utn/web/app/sec/utnwebappsec/UtnWebAppSecApplicationTests.java
@@ -0,0 +1,13 @@
+package org.utn.web.app.sec.utnwebappsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtnWebAppSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
