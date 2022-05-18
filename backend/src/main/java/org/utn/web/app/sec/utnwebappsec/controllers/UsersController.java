a55ae6fffd164a4d66bfe72d9f913805087870f9
Francisco Enzo Di Giorgio <f.e.digiorgio@gmail.com>
2022-05-18 11:33:08 -0300

    Logind and get private endpoint

--- backend/src/main/java/org/utn/web/app/sec/utnwebappsec/controllers/UsersController.java
+++ backend/src/main/java/org/utn/web/app/sec/utnwebappsec/controllers/UsersController.java
@@ -1,8 +1,11 @@
 package org.utn.web.app.sec.utnwebappsec.controllers;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.utn.web.app.sec.utnwebappsec.dtos.LoginRequest;
+import org.utn.web.app.sec.utnwebappsec.dtos.LoginResponse;
 import org.utn.web.app.sec.utnwebappsec.dtos.UserDto;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,5 +21,15 @@ public class UsersController {
         List<UserDto> response = new ArrayList<>();
         response.add(new UserDto("fdigiorgio", "Francisco Di Giorgio", "fdigiorgio@frba.utn.edu.ar"));
         return response;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public LoginResponse login(@RequestBody LoginRequest request) {
+        return new LoginResponse("harcode-token");
+    }
+
+    @RequestMapping(path = "/private", method = RequestMethod.GET)
+    public UserDto getPrivate() {
+        return new UserDto("fdigiorgio", "Francisco Di Giorgio", "fdigiorgio@frba.utn.edu.ar");
     }
 }
