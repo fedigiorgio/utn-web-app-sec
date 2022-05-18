ee9530eabe46782eeb232e1952f042c731d9180c
Francisco Enzo Di Giorgio <f.e.digiorgio@gmail.com>
2022-05-18 12:00:00 -0300

    Implementation for getAll endpoint and dummy login

--- backend/src/main/java/org/utn/web/app/sec/utnwebappsec/controllers/UsersController.java
+++ backend/src/main/java/org/utn/web/app/sec/utnwebappsec/controllers/UsersController.java
@@ -9,23 +9,43 @@ import org.utn.web.app.sec.utnwebappsec.dtos.LoginResponse;
 import org.utn.web.app.sec.utnwebappsec.dtos.UserDto;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
 
+    private String CONNECTION_URL = "overrides_locally";
+
     @RequestMapping(method = RequestMethod.GET)
     public List<UserDto> getAll(HttpServletRequest request) {
-        List<UserDto> response = new ArrayList<>();
-        response.add(new UserDto("fdigiorgio", "Francisco Di Giorgio", "fdigiorgio@frba.utn.edu.ar"));
-        return response;
+
+        List<UserDto> users = new ArrayList<>();
+
+        try (Connection con = DriverManager.getConnection(CONNECTION_URL);
+             PreparedStatement ps = con.prepareStatement("SELECT * FROM dbo.USERS")) {
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    users.add(new UserDto(rs.getString("USERNAME"), rs.getString("FULLNAME"), rs.getString("MAIL")));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public LoginResponse login(@RequestBody LoginRequest request) {
-        return new LoginResponse("harcode-token");
+    public LoginResponse login(@RequestBody LoginRequest request) throws Exception {
+        if (request.getUserName() != "fdigiorgio" && request.getPassword() != "password123"){
+            throw new Exception();
+        }
+
+        return new LoginResponse("fake-token");
     }
 
     @RequestMapping(path = "/private", method = RequestMethod.GET)
