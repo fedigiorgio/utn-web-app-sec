55ed5daa36d34cdfc57d04fb1bf879388a660e81
Jorge Martinez <Jorge_Martinez@itrsa.com.ar>
2022-06-04 19:26:33 -0300

    implementar validación usuario logueado en las paginas. refactor funciones para obtener usuarios

--- frontEnd/controller.js
+++ frontEnd/controller.js
@@ -6,29 +6,31 @@ const tokenKey = "utn-web-app-sec-token";
 const userKey = "usuario";
 
 function logueo() {
-    //comentar cuando se vaya a usar la app
-    //window.location.replace("file:///C:/Users/Jorge_Martinez/Documents/TPSegWeb/LoginForm/mainPage.html");
-    //user = { username: "jorgemtnz", fullname: "jorge", mail: "jorge@msn.com" };
+    //comentar cuando se vaya a usar la app, es solo para usar sin base de datos
+    storage.setItem(tokenKey, "fgdfhbdgsjgcsjsnmmsrkk");
+    storage.setItem(userKey, $('#Uname').val());
+    window.location.replace("./index.html");
+
     //descomentar cuando se este usando la app
 
-    $.ajax({
-        url: `${host}/users/login`,
-        type: "POST",
-        dataType: "html",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        data: JSON.stringify({ "userName": $('#Uname').val(), "password": $('#Pass').val() }),
-        cache: false,
-        success: function(response) {
-            storage.setItem(tokenKey, JSON.parse(response).token);
-            storage.setItem(userKey, $('#Uname').val());
-            window.location.replace("./index.html");
-        },
-        error: function() {
-            alert("Error al loguearse");
-        }
-    });
+    // $.ajax({
+    //     url: `${host}/users/login`,
+    //     type: "POST",
+    //     dataType: "html",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     data: JSON.stringify({ "userName": $('#Uname').val(), "password": $('#Pass').val() }),
+    //     cache: false,
+    //     success: function(response) {
+    //         storage.setItem(tokenKey, JSON.parse(response).token);
+    //         storage.setItem(userKey, $('#Uname').val());
+    //         window.location.replace("./index.html");
+    //     },
+    //     error: function() {
+    //         alert("Error al loguearse");
+    //     }
+    // });
 }
 
 
@@ -36,32 +38,7 @@ function cargarUsuario() {
     document.getElementById("lblBienvenido").innerHTML = "Bienvenido " + storage.getItem(userKey);
 }
 
-function buscarUsuario() {
-    //comentar cuando se use la app
-    let data = [];
-    //data = [{ username: 'jorgemtnz', fullname: 'jorge martinez', mail: 'jorgeemtnz@gmail.com' }];
-
-    //descomentar cuando se use la app
-
-    const url = `${host}/users?fullName=${$('#Uname').val()}`
-    JSON.stringify(token)
-    $.ajax({
-        url: url,
-        type: "GET",
-        async: false,
-        dataType: "html",
-        headers: {
-            "token": storage.getItem(tokenKey)
-        },
-        cache: false,
-        success: function(response) {
-            data = JSON.parse(response);
-        },
-        error: function() {
-            alert("Error al buscar usuarios");
-        }
-    });
-
+function crearTabla(data) {
     const tableData = data.map(function(value) {
         return (
             `<tr>
@@ -73,53 +50,59 @@ function buscarUsuario() {
     }).join('');
     const tableBody = document.querySelector("#tableBody");
     tableBody.innerHTML = tableData;
-
 }
 
-function crearTablaUsuarios() {
-    //comentar cuando se use la app
+function getUsuarios(url) {
     let data = [];
-    //data = [{ username: 'fdigiorgio', fullname: 'Francisco Di Giorgio', mail: 'fdigiorgio@frba.utn.edu.ar' }, { username: 'jorgemtnz', fullname: 'jorge martinez', mail: 'jorgeemtnz@gmail.com' }];
+    //comentar cuando se use la app, es solo para usar sin base de datos
+    data = [{ username: 'fdigiorgio', fullname: 'Francisco Di Giorgio', mail: 'fdigiorgio@frba.utn.edu.ar' }, { username: 'jorgemtnz', fullname: 'jorge martinez', mail: 'jorgeemtnz@gmail.com' }];
+    alert(url);
 
-    //descomentar cuando se use la app
-    JSON.stringify(token)
-    $.ajax({
-        url: `${host}/users?fullName`,
-        type: "GET",
-        async: false,
-        dataType: "html",
-        headers: {
-            "token": storage.getItem(tokenKey)
-        },
-        cache: false,
-        success: function(response) {
-            data = JSON.parse(response);
-        },
-        error: function() {
-            alert("Error al buscar usuarios");
-        }
-    });
+    // $.ajax({
+    //     url: url,
+    //     type: "GET",
+    //     async: false,
+    //     dataType: "html",
+    //     headers: {
+    //         "token": storage.getItem(tokenKey)
+    //     },
+    //     cache: false,
+    //     success: function(response) {
+    //         data = JSON.parse(response);
+    //     },
+    //     error: function() {
+    //         alert("Error al buscar usuarios");
+    //     }
+    // });
 
-    const tableData = data.map(function(value) {
-        return (
-            `<tr>
-                    <td>${value.username}</td>
-                    <td>${value.fullName}</td>
-                    <td>${value.mail}</td>
-                </tr>`
-        );
-    }).join('');
-    const tableBody = document.querySelector("#tableBody");
-    tableBody.innerHTML = tableData;
+    crearTabla(data);
+}
 
+function buscarUsuario() {
+    const url = `${host}/users?fullName=${$('#Uname').val()}`;
+    getUsuarios(url);
+}
+
+function buscarUsuarios() {
+    const url = `${host}/users?fullName`;
+    getUsuarios(url);
 }
 
 function cerrarSesion() {
+    window.localStorage.removeItem(userKey);
+    window.localStorage.removeItem(tokenKey);
+    window.localStorage.clear();
     alert("Adios");
     // return true or false, depending on whether you want to allow the `href` property to follow through or not
 }
 
 function validarSesion() {
-    alert("sesion valida");
-    // return true or false, depending on whether you want to allow the `href` property to follow through or not
+    let usuario = storage.getItem(userKey);
+    let token = storage.getItem(tokenKey);
+
+    if (usuario == null || token == null) {
+        alert("sesion Invalida");
+        window.localStorage.clear();
+        window.location.replace("./login.html");
+    }
 }
